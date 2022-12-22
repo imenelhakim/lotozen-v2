761686ca2da5effb32c2988c036471e05522d6bb
Imen EL HAKIM <imen.elhakim@esprit.tn>
2022-12-22 14:22:47 +0000

    Fixing errors

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const express = require("express");
+var app = express();
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const userRoute = require("./src/routes/user.route");
 const transactionRoute = require("./src/routes/transaction.route");
 const gameRoute = require("./src/routes/game.route");
 const chestRoute = require("./src/routes/chest.route");
 
+// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static("assets"));
+app.use("/assets", express.static("assets"));
+
 app.use("/user", userRoute);
 app.use("/transaction", transactionRoute);
 app.use("/game", gameRoute);
 app.use("/chest", chestRoute);
 
-app.get('/hello', (req, res) => {
-    return res.status(200).json({"Greetings" : "Hello World!"});
+app.get("/hello", (req, res) => {
+  return res.status(200).json({ Greetings: "Hello World!" });
 });
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.use(express.static('assets'));
-app.use('/assets', express.static('assets'));
 
 module.exports = app;
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,17 @@
-const connection = require('../utils/db');
-const bcrypt =  require('bcryptjs');
-const sendmail = require('../utils/sendmail');
-const {query} = require("express");
+const connection = require("../utils/db");
+const bcrypt = require("bcryptjs");
+const sendmail = require("../utils/sendmail");
+const { query } = require("express");
+var bodyParser = require("body-parser");
 
 exports.Login = (req, res) => {
-    let user = req.body;
-    console.log(req.body.email);
-    const query = "SELECT username, password, coins, money, isActive , ban, paypal from users WHERE email = 'aaaa@aas.com'";
-    connection.query(query, (err, result) => {
-        console.log(result);
-        return res.status(200).json(result);
-    });
-}
+  let user = req.body.email;
+  // console.log(req.body.email);
+  console.log(user);
+  var query =
+    "SELECT username, password, coins, money, isActive , ban, paypal from users WHERE email = ?";
+  connection.query(query, user, (err, result) => {
+    console.log(result);
+    return res.status(200).json(result);
+  });
+};
